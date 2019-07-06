5018332d2d7e5c5189e3022c78662e59d468d47a
Ousmane Ndiaye <ousmane.ndiaye6@orange-sonatel.com>
2019-07-06 17:20:49 +0000

    correct integration

---
+++ src/main/resources/static/css_template_index/js/script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+	'use strict';
+
+	$('.progression-studios-slider').flexslider({
+		slideshow: true,
+		slideshowSpeed: 8000,
+		animation: 'fade',
+		animationSpeed: 800,
+		directionNav: true,
+		controlNav: true,
+		prevText: '',
+		nextText: ''
+	});
+});
